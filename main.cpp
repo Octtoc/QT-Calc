aef6df105b67d76e1f3ddbaac8ffb09cff6b5fb5
Michael Bubeck <Michael Bubeck@UB-Software.local>
2016-06-09 11:08:26 +0200

    Taborder + Stylesheet in Desginer

--- main.cpp
+++ main.cpp
@@ -6,16 +6,6 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QFile file("C:\\temp\\test.txt");
-    QTextStream in(&file);
-
-    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-        return a.exec();
-
-    QString stylesheet = in.readAll();
-    a.setStyleSheet(stylesheet);
-    file.close();
-
     MainWindow w;
     w.show();
 
